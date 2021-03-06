5822540f37aa9f2901e8f362687504ebb7fcd20f
KACPER CIERPIAK <KACPER_CIERPIAK@jabil.com>
2021-03-06 18:10:24 +0100

    Zmiana nazwy projektu na autorow projektu

--- Program.cs
+++ Program.cs
@@ -6,8 +6,9 @@ namespace TestGit
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("GitHubLab");
-            Console.WriteLine("Version: {0}", Environment.Version.ToString()); ;
+            var dev1 = new DEV1();
+            var dev2 = new DEV2();
+            Console.WriteLine("Developers: {0} {1}", dev1.author, dev2.author);
         }
     }
 }
