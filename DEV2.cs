3cd682f0ac7857094afc07ea33c3815c3677be7b
Arkadiusz Biesiada <arkbie002@o365.student.utp.edu.pl>
2021-03-06 17:45:41 +0100

    Nowa klasa DEV2

---
+++ DEV2.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TestGit
+{
+    class DEV2
+
+    {
+        public string author { get; } = "Arkadiusz Biesiada";
+    }
+}
